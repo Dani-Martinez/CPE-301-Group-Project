9138e3181e947745a812d474e912d6e21de856ee
brantley-aaron <66802849+brantley-aaron@users.noreply.github.com>
2021-05-02 17:19:42 -0700

    Temperature monitor using thermistor. Still need to create a delay function from scratch.

--- tempMonitor.ino
+++ tempMonitor.ino
@@ -1,18 +1,15 @@
 #define thermistorPin 0
 
-void setup() {
-Serial.begin(9600);
+void setup () 
+{
+  Serial.begin(9600);
 }
 
-void loop() {
-
-  int signal = analogRead(thermistorPin);
+void loop () 
+{
+  int signal = analogRead (thermistorPin);
   float thermistor = 10000 * (1023.0 / (float) signal - 1.0);
-  float temperature = (1.0 / (1.009249522e-03 + 2.378405444e-04 * log (thermistor) + 2.019202697e-07 * log (thermistor)*log (thermistor) *log (thermistor)));
-  temperature -= 273.15;
-  temperature *= 9.0;
-  temperature /= 5.0; 
-  temperature += 32.0; 
+  float temperature = (((((1.0 / (1.009249522e-03 + 2.378405444e-04 * log (thermistor) + 2.019202697e-07 * log (thermistor)*log (thermistor) *log (thermistor))) - 273.15) * 9.0) / 5.0) + 32.0);
 
   Serial.print("Temperature: "); 
   Serial.print(temperature);
