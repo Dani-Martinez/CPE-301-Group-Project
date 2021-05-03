5d0b875e428c042c24c0831519fbc79bdfbaaf7f
brantley-aaron <66802849+brantley-aaron@users.noreply.github.com>
2021-05-03 00:23:36 -0700

    printTemperature () prints the current temperature from the thermistor.

--- tempMonitor.ino
+++ tempMonitor.ino
@@ -7,13 +7,18 @@ void setup ()
 
 void loop () 
 {
+  printTemperature ();
+
+  delay (500);
+}
+
+void printTemperature ()
+{
   int signal = analogRead (thermistorPin);
   float thermistor = 10000 * (1023.0 / (float) signal - 1.0);
   float temperature = (((((1.0 / (1.009249522e-03 + 2.378405444e-04 * log (thermistor) + 2.019202697e-07 * log (thermistor)*log (thermistor) *log (thermistor))) - 273.15) * 9.0) / 5.0) + 32.0);
 
   Serial.print("Temperature: "); 
   Serial.print(temperature);
-  Serial.println(" degrees"); 
-
-  delay(500); // need to create delay function from scratch
+  Serial.println(" degrees");
 }
